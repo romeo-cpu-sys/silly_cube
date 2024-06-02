27ba7408dc991a076961208c5835248c38f957e7
romeo-cpu-sys <158520698+romeo-cpu-sys@users.noreply.github.com>
2024-06-02 15:46:34 -0400

    Update dmc.cpp

--- dmc.cpp
+++ dmc.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
 
-int main() {
-    int* ptr = new int;
-
-    *ptr = 100;
-
-    std::cout << "Value of your variable is: " << *ptr << std::endl;
-
-    delete ptr;
-
-    return 0;
-} 
